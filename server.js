9314dcf29e892dae6ff1e4ac7d141297b2e78a57
ceriwestcott <ceribusiness@gmail.com>
2023-11-17 21:28:21 +0000

    send files updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/nwp-rd-dashboard'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/nwp-rd-dashboard/index.html'));
+res.sendFile(path.join(__dirname+'/dist/browser/nwp-rd-dashboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
